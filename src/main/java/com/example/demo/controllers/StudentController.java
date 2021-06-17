1324ed081a4463f89e25030ad1cd7dd176ade110
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-06-17 19:57:55 +0530

    updated!

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Students;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -20,6 +27,32 @@ public class StudentController {
         return new ModelAndView("index.html");
     }
 
+    @RequestMapping("/students")
+    public String listStudents(HttpServletRequest request, Model model){
+        List<Students> studentsList = new ArrayList<>();
+
+        studentsList.add(new Students("Isuru",24));
+        studentsList.add(new Students("Rajitha",24));
+
+        model.addAttribute("studentList", studentsList);
+        return "students";
 
 
+    }
+
+
+    @RequestMapping("/create")
+    public String createStudent(@ModelAttribute Students students,Model model){
+        System.out.println(students.getName());
+        System.out.println(students.getAge());
+
+        return "greeting";
+    }
+
+    @RequestMapping("/new")
+    public String newStudent(Model model) {
+        Students students = new Students();
+        model.addAttribute("students", students);
+        return "form";
+    }
 }
